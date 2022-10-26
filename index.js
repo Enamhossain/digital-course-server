c75d094008726e318a916d766638f69764210f1e
Enamhossain <bhaianam9@gmail.com>
2022-10-26 20:24:01 +0600

    category data set

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const categories = require('./Data/Category.js');
+const news = require('./data/news.json');
 
 
 app.get('/',(req,res) =>{
