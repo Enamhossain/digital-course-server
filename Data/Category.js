3b24acb2d2567cec051b63a413218d22793b91c6
Enamhossain <bhaianam9@gmail.com>
2022-10-26 19:46:44 +0600

    server data add

---
+++ Data/Category.js
@@ -0,0 +1,34 @@
+[
+    {
+      "id": "01",
+      "name": "Data Science "
+    },
+    {
+      "id": "02",
+      "name": "Programing Langues"
+    },
+    {
+      "id": "03",
+      "name": "Blockchain"
+    },
+    {
+      "id": "04",
+      "name": "Cyber Security"
+    },
+    {
+      "id": "05",
+      "name": "Web Development"
+    },
+    {
+      "id": "06",
+      "name": "Cloud Computing"
+    },
+    {
+      "id": "07",
+      "name": "Machine Learning"
+    },
+    {
+      "id": "08",
+      "name": "All Courses"
+    }
+  ]
---
+++ Data/Course.js
@@ -0,0 +1,99 @@
+[
+    //Data Science 
+    {
+        "_id": "0282e0e58a5c404fbd15261f11c2ab6a",
+        "category_id": "01",
+        "title": "Introduction to Database and SQL",
+
+        "name": "Mr. Udit Mehrotra",
+        "img": "https://images.unsplash.com/photo-1632406898177-95f7acd8854f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGRhdGFiYXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        "thumbnail_url": "",
+        "image_url": "",
+        "details": "Databases are an essential part of any web or cloud application in the industry today. Most prominent software development companies have specific questions about this topic during their interviews. In this course, we'll be learning about why it's important to know SQL coding, followed by how we can formulate a SQL Solution for any problem. We will be understanding the steps for installation of MySQL for both MacOS and Windows operating systems and also how data is stored in MySQL. We'll then be looking at the various data manipulation concepts such as fetching data, filtering data, aggregating data, and manipulating data. We will also explore the concept of joins and understand the order of precedence in which the compiler executes SQL Commands"
+    },
+    {
+        "_id": "0283f0e58a5c404fbd15261f11c2ab6a",
+        "category_id": "02",
+        "title": "Data Structures & Algorithms in Java",
+
+        "name": "Adity Roy",
+        "img": "https://images.unsplash.com/photo-1523961131990-5ea7c61b2107?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dGVjaHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+        ,
+        "thumbnail_url": "",
+        "image_url": "",
+        "details": "Learn to store and organize data efficiently through this free Data Structures and Algorithms in Java course. You will first learn about data structures and their significance. You will then get familiar with algorithms and time complexity. Further, you will understand recursion in depth. You will learn about the recursive functions, recursive tree, and internal mechanism of recursionThe example of the Tower of Hanoi will help you comprehend it better. Lastly, you will go through sorting algorithms in detail. Enroll in this free Data Structures and Algorithms in Java course and gain a free certificate. Realize your career goal of acquiring advanced software skills with Great Learning's well-established Software Development Courses. Enroll in the top programs of your interest and achieve certificates of course completion"
+    },
+      // Programing Langues
+    {
+        "_id": "0284g0e58a5c404fbd15261f11c2ab6a",
+        "category_id": "03",
+        "title": "Java Programming",
+        "name": "Ahsan Habib",
+        "img": "https://images.unsplash.com/photo-1533709752211-118fcaf03312?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8amF2YSUyMHByb2dyYW1taW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        "image_url": "",
+        "details": "This Java online course with a certificate is a good start for beginners who want to learn the basics of Java. You will first get briefly introduced to the Java Programming language, followed by information on Java IDE installation. You will go through different concepts like variables, data types, operators, arrays, flow control statements, and functions with appropriate code examples.You will also have the opportunity to get started with the first Java program in detail and step into the initial stage of Java programming. Enroll in this free Java online course, complete the quiz, and gain a free certificate of course completion.Interested in learning advanced software skills? Great Learning offers top-class Software Development Courses. Enroll in the paid programs of your choice and earn highly valued certificates."
+    },
+  
+    {
+        "_id": "0285i0e58a5c404fbd15261f11c2ab6a",
+        "category_id": "04",
+        "title": "Python Foundations",
+        "name": "Mohammad khaled",
+        "img": "https://images.unsplash.com/photo-1526379095098-d400fd0bf935?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHl0aG9uJTIwcHJvZ3JhbW1pbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        
+        "image_url": "",
+        "details": "Python is one of the easiest to learn and widely used programming languages. This course will guide you through the basics of Python programming. Starting from what python is to why it is needed will be discussed through the course. An introduction to different IDEs that can be used for programming in Python is also included along with various other concepts of Python like, variables, literal, and collections."
+    },
+
+    //Blockchain
+
+    {
+        "_id": "0286i0e58a5c404fbd15261f11c2ab6a",
+        "category_id": "05",
+        "title": "Blockchain Basics",
+        "name": "Jim wick",
+        "img": "https://images.unsplash.com/photo-1526378800651-c32d170fe6f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGJsb2NrY2hhaW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        
+        "image_url": "",
+        "details": "This free Blockchain course for beginners aims at making the concept easier for beginners by empowering them with the more robust basics. Firstly, you will get an idea of Blockchain through its comprehensive introduction. You will then get familiarized with important concepts like cryptography, the role of intermediaries, consensus mechanism, proof of work and stake, and transaction mechanism. The course also focuses on the Blockchain ecosystem, its need in industries, and its challenges during its adoption. It is explained through practical examples to give you a clear idea of Blockchain. Enroll in this free course to start with Blockchain, crack the quiz, and earn a free course completion certificate."
+    },
+    {
+        "_id": "0287i0e58a5c404fbd15261f11c2ab6a",
+        "category_id": "06",
+        "title": "Data Structures in C",
+        "name": "Dr.John ",
+        "img": "https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGF0YSUyMHN0cnVjdHVyZSUyMGN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        
+        "image_url": "",
+        "details": "This free course focuses on providing learners with solid wisdom in the basics of Data Structures in C, as data structures are the building blocks of any programming language. You will get familiar with an array, stack, queue, and linked list. Further, you will learn binary tree, binary search tree, heap, and hashing. You will understand these concepts better with the appropriate examples and hands-on demos. Enroll in this free course and complete the suggested modules and a quiz to earn a free certificate. Are you up for stepping into advancing your software skills? Great Learning offers highly appreciated Online Software Engineering Courses that will help you build your career. Enroll in the paid program of your interest and achieve a certificate. "
+    },
+
+   //Cyber Security
+
+   {
+    "_id": "0288i0e58a5c404fbd15261f11c2ab6a",
+    "category_id": "07",
+    "title": "Introduction to Information Security",
+    "name": "Dr.John ",
+    "img": "https://images.unsplash.com/photo-1614064641938-3bbee52942c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c2VjdXJpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    
+    "image_url": "",
+    "details": "This Introduction to Information Security course is designed to give you insights on various aspects of securing your data. The course begins with briefing the attacker's lifecycle and continues with explaining different case studies on popular companies, attack type, cause, and the ways they could have been prevented. The course then explains breaches such as target breaches and password breaches. It takes you through crucial attack vectors, defenses like holistic defense, security scorecards, do's and don'ts to save passwords, hash and its methods, custom hardware, control-flow integrity, and cryptographic approach for control flow integrity concepts. There is an assessment to evaluate your knowledge at the end of the course. Complete the course for free and avail your certificate. You can also study the attached materials for reference "
+},
+   {
+    "_id": "0289i0e58a5c404fbd15261f11c2ab6a",
+    "category_id": "08",
+    "title": "Introduction to Ethical Hacking",
+    "name": "Dr.John ",
+    "img": "https://images.unsplash.com/photo-1541728472741-03e45a58cf88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aGFja2luZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    
+    "image_url": "",
+    "details": "The term ethical hacking, more commonly referred to as White Hat in the industry today, is a skill that ranks among the top 3 skills. Organizations and corporations must ensure that their infrastructure and systems are secured and breaches/attacks are kept at bay. Ethical Hacking takes you from the starting point through the finishing point of everything you need to know about this domain and starts the journey to master it. This free introduction to ethical hacking course online gives you insight into its functions under the top 3 domains in the industry today. This course gives you the scoop into the foundations, processes, domains, and outcomes of Ethical Hacking. You will understand the domain, its common attacks, and hacking methodology in every ethical hacking domain. "
+},
+
+
+
+
+
+
+]
