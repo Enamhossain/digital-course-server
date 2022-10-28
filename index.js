3cee69fa02bc73f78143f1c072518f3cb76dae1b
Enamhossain <bhaianam9@gmail.com>
2022-10-28 20:52:07 +0600

    vercel add

--- index.js
+++ index.js
@@ -5,14 +5,37 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./Data/Category.js');
-const news = require('./data/news.json');
+const categories = require('./Data/Category.json');
+const course = require('./Data/Course.json')
 
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
+});
 
-app.get('/',(req,res) =>{
-    res.send("Course APi Runnig")
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(course);
+    }
+    else {
+        const category_course = course.filter(n => n.category_id === id);
+        res.send(category_course);
+    }
 })
 
+app.get('/course', (req, res) =>{
+    res.send(course);
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectCourses = course.find(n => n._id === id);
+    res.send(selectCourses);
+    console.log(req.params)
+});
+
+
+
 app.listen(port, () => {
-    console.log('Dragon News Server running on port', port);
+    console.log("digital course running", port);
 })
